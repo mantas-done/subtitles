d73b4dd0aaa1c4f4584e99f3ab629ace7584ac49
Mantas <mantas783@gmail.com>
2017-01-14 19:43:05 +0200

    test update

--- tests/formats/SrtTest.php
+++ tests/formats/SrtTest.php
@@ -4,6 +4,8 @@ use Done\Subtitles\Subtitles;
 
 class SrtTest extends SubtitleCase {
 
+    protected $format = 'srt';
+
     public function testConvertingFileFromSrtToSrtDoesNotChangeItContent()
     {
         $srt_path = './tests/files/srt.srt';
@@ -15,6 +17,49 @@ class SrtTest extends SubtitleCase {
         $this->assertFileEquals($srt_path, $temporary_srt_path);
 
         unlink($temporary_srt_path);
+    }
+
+    public function testFileToInternalFormat()
+    {
+        $actual_internal_format = Subtitles::load(self::fileContent(), $this->format)->getInternalFormat();
+
+        $this->assertInternalFormatsEqual(self::generatedSubtitles()->getInternalFormat(), $actual_internal_format);
+    }
+
+    public function testConvertToFile()
+    {
+        $actual_file_content = self::generatedSubtitles()->content($this->format);
+
+        $this->assertEquals(self::fileContent(), $actual_file_content);
+    }
+
+    // @TODO test time above 1 hour
+
+    // ---------------------------------- private ----------------------------------------------------------------------
+
+    private static function fileContent()
+    {
+        $content = <<< TEXT
+1
+00:02:17,440 --> 00:02:20,375
+Senator, we're making
+our final approach into Coruscant.
+
+2
+00:02:20,476 --> 00:02:22,501
+Very good, Lieutenant.
+TEXT;
+        $content = str_replace("\r", '', $content);
+
+        return $content;
+
+    }
+
+    private static function generatedSubtitles()
+    {
+        return (new Subtitles())
+            ->add(137.44, 140.375, ['Senator, we\'re making', 'our final approach into Coruscant.'])
+            ->add(140.476, 142.501, ['Very good, Lieutenant.']);
     }
 
 }
