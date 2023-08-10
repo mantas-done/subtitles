54b586f36fa472e3ec1ada1ed0c5306ff1817361
Mantas <mantas783@gmail.com>
2023-08-10 20:07:27 +0300

    Improve XML parsing

--- tests/formats/TtmlTest.php
+++ tests/formats/TtmlTest.php
@@ -211,4 +211,89 @@ X;
         $expected = (new Subtitles())->add(0, 5.38, 'test1')->add(5.38, 6.38, 'test2')->getInternalFormat();
         $this->assertInternalFormatsEqual($expected, $actual);
     }
+
+    public function testParsesDCSubtitles()
+    {
+        $text = <<<X
+<?xml version="1.0" encoding="UTF-8"?>
+<DCSubtitle Version="1.0">
+   <!--Monal 2008 - v6.1.1.1-->
+   <!--Sync - "FFP" - 00:00:00:00-->
+   <!--Running Speed - 24fps-->
+   <SubtitleID>5c652e39-9bf1-4c67-9793-0ec7c8948e83</SubtitleID>
+   <MovieTitle>MAL DE PIERRES</MovieTitle>
+   <ReelNumber>1AB</ReelNumber>
+   <Language>English</Language>
+   <LoadFont Id="Font1" URI="1AB-dci.ttf" />
+   <Font Effect="border" Size="42" Id="Font1">
+      <Subtitle SpotNumber="1" TimeIn="00:00:51:000" TimeOut="00:00:52:000">
+         <Text HAlign="center" HPosition="0.0" VAlign="bottom" VPosition="7">
+            <Font>Here.</Font>
+         </Text>
+      </Subtitle>
+      <Subtitle SpotNumber="2" TimeIn="00:01:30:020" TimeOut="00:01:31:031">
+         <Text HAlign="center" HPosition="0.0" VAlign="bottom" VPosition="7">
+            <Font>No, not now.</Font>
+         </Text>
+      </Subtitle>
+   </Font>
+</DCSubtitle>
+X;
+        $actual = Subtitles::loadFromString($text)->getInternalFormat();
+        $expected = (new Subtitles())->add(51, 52, 'Here.')->add(90.02, 91.031, 'No, not now.')->getInternalFormat();
+        $this->assertInternalFormatsEqual($expected, $actual);
+    }
+
+    public function testConvertFromXml5()
+    {
+        $text = <<<X
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Profile: EBU-TT-D-Basic-DE -->
+<tt:tt
+	xmlns:tt="http://www.w3.org/ns/ttml"
+	xmlns:tts="http://www.w3.org/ns/ttml#styling"
+	xmlns:xs="http://www.w3.org/2001/XMLSchema"
+	xmlns:ttm="http://www.w3.org/ns/ttml#metadata"
+	xmlns:ttp="http://www.w3.org/ns/ttml#parameter"
+	xmlns:ebuttdt="urn:ebu:tt:datatypes"
+	xmlns:ebuttm="urn:ebu:tt:metadata"
+	xmlns:ebutts="urn:ebu:tt:style"
+	xmlns:ebuttExt="urn:ebu:tt:extension" ttp:timeBase="media" ttp:cellResolution="50 30" xml:lang="de">
+	<tt:head>
+		<tt:metadata>
+			<ebuttm:documentMetadata>
+				<ebuttm:documentEbuttVersion>v1.0</ebuttm:documentEbuttVersion>
+			</ebuttm:documentMetadata>
+		</tt:metadata>
+		<tt:styling>
+			<tt:style xml:id="S1" tts:fontSize="160%" tts:fontFamily="Verdana, Arial, Tiresias" tts:lineHeight="125%" />
+			<tt:style xml:id="S2" tts:textAlign="center" />
+			<tt:style xml:id="S3" tts:color="#ffffff" tts:backgroundColor="#000000c2" />
+			<tt:style xml:id="S4" tts:color="#ffff00" tts:backgroundColor="#000000c2" />
+		</tt:styling>
+		<tt:layout>
+			<tt:region xml:id="R1" tts:origin="10% 83%" tts:extent="80% 7%" />
+			<tt:region xml:id="R2" tts:origin="10% 77%" tts:extent="80% 13%" />
+			<tt:region xml:id="R3" tts:origin="10% 70%" tts:extent="80% 7%" />
+			<tt:region xml:id="R4" tts:origin="10% 63%" tts:extent="80% 14%" />
+		</tt:layout>
+	</tt:head>
+	<tt:body>
+		<tt:div style="S1">
+			<tt:p xml:id="C1" region="R1" style="S2" begin="00:00:00.000" end="00:00:02.080">
+				<tt:span style="S3">Badewetter an der Ostsee.</tt:span>
+			</tt:p>
+			<tt:p xml:id="C2" region="R2" style="S2" begin="00:00:02.200" end="00:00:05.400">
+				<tt:span style="S3">Jetzt im Sommer</tt:span>
+				<tt:br />
+				<tt:span style="S3">hat das Meer um die 19°C.</tt:span>
+			</tt:p>
+        </tt:div>
+	</tt:body>
+</tt:tt>
+X;
+        $actual = Subtitles::loadFromString($text)->getInternalFormat();
+        $expected = (new Subtitles())->add(0, 2.08, 'Badewetter an der Ostsee.')->add(2.2, 5.4, ['Jetzt im Sommer', 'hat das Meer um die 19°C.'])->getInternalFormat();
+        $this->assertInternalFormatsEqual($expected, $actual);
+    }
 }
